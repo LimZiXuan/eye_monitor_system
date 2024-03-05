b8f06ca79999b8f48608dda7862c9eb09952f9a0
DESKTOP-B94IAJ8\LEGION <limyikai441@gmail.com>
2024-03-06 01:53:18 +0800

    Update eyetraining point system & record to firebase

--- lib/eye_training.dart
+++ lib/eye_training.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EyeTraining extends StatefulWidget {
   const EyeTraining({Key? key}) : super(key: key);
@@ -41,6 +43,14 @@ class _EyeGameState extends State<EyeGame> {
   double starX = 0;
   double starY = 0;
   bool showStar = false;
+  int points = 0;
+  int stars = 0;
+  final int totalStars = 10;
+  bool collisionDetected = false;
+  bool allstarsShowed = false;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final uid = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   void initState() {
@@ -57,13 +67,38 @@ class _EyeGameState extends State<EyeGame> {
   void startGame() {
     _starTimer = Timer.periodic(Duration(seconds: 2), (timer) {
       setState(() {
-        showStar = true;
-        generateStarPosition();
-        _showTimer = Timer(Duration(seconds: 1), () {
-          setState(() {
-            showStar = false;
+        if (points < totalStars ~/ 2) {
+          generateStarPosition();
+          _showTimer = Timer(Duration(seconds: 1), () {
+            setState(() {
+              showStar = false;
+              collisionDetected =
+                  false; // Reset collision detection for next star
+            });
           });
-        });
+        } else {
+          _starTimer?.cancel();
+          _showTimer?.cancel();
+          recordTestResult(points >= totalStars ~/ 2);
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Test Completed'),
+              content: Text(points >= totalStars ~/ 2
+                  ? 'You have successfully completed the test.'
+                  : 'You have failed the test.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog
+                    Navigator.pop(context); // Return to previous page
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
       });
     });
   }
@@ -72,6 +107,34 @@ class _EyeGameState extends State<EyeGame> {
     setState(() {
       starX = Random().nextDouble() * screenWidth;
       starY = Random().nextDouble() * screenHeight;
+      showStar = true;
+      collisionDetected = false; // Reset collision detection for new star
+      stars++;
+      if (stars == 10) {
+        allstarsShowed = true;
+      }
+
+      if (allstarsShowed && (points < totalStars ~/ 2)) {
+        _starTimer?.cancel();
+        _showTimer?.cancel();
+        recordTestResult(false); // Record test result as fail
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Test Completed'),
+            content: Text('You have failed the test.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context); // Close the dialog
+                  Navigator.pop(context); // Return to previous page
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
     });
   }
 
@@ -84,11 +147,56 @@ class _EyeGameState extends State<EyeGame> {
   }
 
   void checkCollision() {
-    if ((ballX <= starX + 50 && ballX >= starX - 50) &&
+    if (!collisionDetected &&
+        (ballX <= starX + 50 && ballX >= starX - 50) &&
         (ballY <= starY + 50 && ballY >= starY - 50)) {
       setState(() {
         showStar = false;
+        points++;
+        collisionDetected = true; // Set collision detected flag
       });
+    }
+    if (!allstarsShowed && points >= totalStars ~/ 2) {
+      _starTimer?.cancel();
+      _showTimer?.cancel();
+      recordTestResult(true); // Record test result as pass
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Test Completed'),
+          content: Text('You have successfully completed the test.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+                Navigator.pop(context); // Return to previous page
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void recordTestResult(bool pass) async {
+    try {
+      User? user = auth.currentUser;
+      if (user != null) {
+        await _firestore
+            .collection('User')
+            .doc(user.uid)
+            .collection('TestHistory')
+            .add({
+          'testType': 'Eye Training',
+          'pass': pass,
+          'timestamp': DateTime.now(),
+        });
+
+        print('Eye Training test result recorded successfully!');
+      }
+    } catch (e) {
+      print('Error recording eye training test result: $e');
     }
   }
 
@@ -109,6 +217,14 @@ class _EyeGameState extends State<EyeGame> {
         children: [
           Container(
             color: Colors.black,
+          ),
+          Positioned(
+            left: 10,
+            top: 10,
+            child: Text(
+              'Points: $points',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
           if (showStar)
             Positioned(
